d47d8dfaab33115559725ab3b3fd8d4c616631e2
iambin6 <miraisan2512@gmail.com>
2025-10-02 23:16:53 +0700

    LAB2

---
+++ LAB2/pizza-app/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css';
+import PizzaHouse from './Components/PizzaHouse';
+
+function App() {
+  return (
+    <div>
+      <PizzaHouse />
+    </div>
+  );
+}
+
+export default App;
